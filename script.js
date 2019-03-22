f801c22b394c4e79d00c17f470e8868c1111190a
Michal0999 <mkonatkowski@gmail.com>
2019-03-22 09:31:16 +0100

    2.5

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let btn = document.getElementById("scrollDown");
-let toLearn = document.getElementById("toLearn");
+// let btn = document.querySelector('.scrollDown')
+// let toLearn = document.querySelector('.toLearn');
 
-btn.addEventListener('click', function () {
+// btn.addEventListener('click', function () {
 
- toLearn.scrollIntoView(false);
-})
+//     toLearn.scrollIntoView(false);
+// })
+
+jQuery(function ($) {
+    $.scrollTo(0);
+
+    $('.scrollUp').click(function () {
+        $.scrollTo($('body'), 1000);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) $('.scrollUp').fadeIn();
+        else $('.scrollUp').fadeOut();
+    });
+
+    $('.scrollDown').click(function () {
+        $.scrollTo((917), 1000);
+    });
+});
